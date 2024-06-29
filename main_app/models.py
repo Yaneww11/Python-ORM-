d3467ebb23dbea27298a1ee646e5bc21a702d4e1
Yaneww11 <93374652+Yaneww11@users.noreply.github.com>
2024-06-29 16:59:47 +0300

    edit name max_length

--- main_app/models.py
+++ main_app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Person(models.Model):
     name = models.CharField(
-        max_length=30
+        max_length=31
     )
 
     age = models.PositiveIntegerField()
